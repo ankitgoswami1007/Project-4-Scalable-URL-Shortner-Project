dd6d9ea6c7cf7b04da7e0ab1ce0862f948261984
Ankit-Goswami <ankitgoswami.work@gmail.com>
2023-05-22 17:58:50 +0530

    today

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("MongoDb is Connected"))
   .catch((err) => console.log(err));
 
-app.use('/apiV1', route);
+// app.use('/apiV1', route);
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"../client/build","index.html"))
 })
